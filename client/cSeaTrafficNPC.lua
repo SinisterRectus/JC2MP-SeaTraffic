22576b11eaff74d95d2f6505649804c8aa3fa990
SinisterRectus <SinisterRectus@users.noreply.github.com>
2016-01-18 11:18:14 -0500

    Improved velocity and prediction smoothing

--- client/cSeaTrafficNPC.lua
+++ client/cSeaTrafficNPC.lua
@@ -3,23 +3,28 @@ class 'SeaTrafficNPC'
 function SeaTrafficNPC:__init(args)
 	
 	self.vehicle = args.entity
+	
+	local position = self:GetPosition()
+	local angle = self:GetAngle()
 
 	self.actor = ClientActor.Create(AssetLocation.Game, {
 		model_id = 26,
-		position = self:GetPosition(),
-		angle = self:GetAngle()
+		position = position,
+		angle = angle
 	})
 	
 	self.path = self.vehicle:GetValue("STPath")
 	self.distance = self.path[1]:Distance(self.path[3])
 	self.distance = self.distance == 0 and 2 * SeaTrafficManager.map.h or self.distance	
 	
-	self.t = self.vehicle:GetValue("STT")
-	self.network_t = self.t
+	self.local_t = self.vehicle:GetValue("STT")
+	self.network_t = self.local_t
+	self.predicted_t = self.local_t
+	
 	self.top_speed = settings.speeds[self.vehicle:GetModelId()]
 	self.speed = self.top_speed * settings.speed_gain
+	self.prev_position = position
 
-	self.timers = {tick = Timer()}
 	self.loader = Events:Subscribe("PostTick", self, self.Load)
 
 end
@@ -52,11 +57,14 @@ function SeaTrafficNPC:Tick(dt)
 	local vehicle = self.vehicle
 	local min = math.min
 	
-	self.t = self.t + speed * dt / self.distance
+	local h = speed * dt / self.distance
+	self.local_t = self.local_t + h + dt * (self.predicted_t - self.local_t)
+	self.predicted_t = self.predicted_t + h
 	
-	while self.t >= 1 do
-
-		self.t = self.t - 1
+	while self.local_t >= 1 or self.predicted_t >= 1 do
+	
+		self.local_t = self.local_t - 1
+		self.predicted_t = self.predicted_t - 1
 
 		path[1] = path[3]
 		path[2] = path[4]
@@ -64,26 +72,33 @@ function SeaTrafficNPC:Tick(dt)
 		path[4] = path[6]
 		-- path 5 and 6 come from server
 		self.distance = path[1]:Distance(path[3])
-		self.distance = self.distance == 0 and 2 * SeaTrafficManager.map.h or self.distance	
+		self.distance = self.distance == 0 and 2 * SeaTrafficManager.map.h or self.distance
 		
 	end	
 		
 	local p1 = vehicle:GetPosition()
 	local q1 = vehicle:GetAngle()
-	local p2, q2 = math.bezier(path[1], path[2], path[4] - 2 * (path[4] - path[3]), path[3], self.t)
+	local p2, q2 = math.bezier(path[1], path[2], path[4] - 2 * (path[4] - path[3]), path[3], self.local_t)
 	
-	p2.y = p1.y; q2.roll = q1.roll; q2.pitch = q1.pitch
+	local v1 = vehicle:GetLinearVelocity()
+	local v2 = (p2 - self.prev_position) / dt
+	local dv = v1:Distance(v2)
+	self.prev_position = Copy(p2)
+	
+	p2.y = p1.y; q2.roll = q1.roll; q2.pitch = q1.pitch; v2.y = v1.y
 	
 	local dp = p1:Distance(p2)
 	local dq = q1:Delta(q2)
 
-	local p = (IsNaN(dp) or dp == 0) and p1 or math.lerp(p1, p2,min((dp + speed) * dt / dp, 1))
+	local v = (IsNaN(dv) or dv == 0) and v1 or math.lerp(v1, v2, min(2 * dt / dv, 1))
+	local p = (IsNaN(dp) or dp == 0) and p1 or math.lerp(p1, p2, min((v:Length() + dp) * dt / dp, 1))
 	local q = (IsNaN(dq) or dq == 0) and q1 or Angle.Slerp(q1, q2, min(2 * dt / dq, 1))
 
+	vehicle:SetLinearVelocity(v)
 	vehicle:SetPosition(p)
 	vehicle:SetAngle(q)
 	
-	self.actor:SetInput(Action.Accelerate, min((dp / dt) / self.top_speed, 1))
+	self.actor:SetInput(Action.Accelerate, min(v:Length() / self.top_speed, 1))
 	
 end
 
@@ -129,6 +144,10 @@ function SeaTrafficNPC:GetNetworkAngle()
 	local _, angle = math.bezier(path[1], path[2], path[4] - 2 * (path[4] - path[3]), path[3], self.network_t)
 	return angle
 
+end
+
+function SeaTrafficNPC:GetLinearVelocity()
+	return self.vehicle:GetLinearVelocity()
 end
 
 function SeaTrafficNPC:GetModelId()
